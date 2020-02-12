ba460973d0b1fe1696d5b82e6e910edfac051fa4
Tamara <Oxian66@mail.ru>
2020-02-12 21:09:13 +0300

    tabs,timer,modal window,slider,calc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', () => {
     'use strict';
+    //tabs
     let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
@@ -19,7 +20,7 @@ for (let i = a; i < tabContent.length; i++) {
         }
     }
 
-    info.addEventListener('click', function(event){
+    info.addEventListener('click', (event) => {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
 for (let i = 0; i < tab.length; i++) {
@@ -32,4 +33,187 @@ for (let i = 0; i < tab.length; i++) {
         }
 
     });
-}); 
+    
+  
+    //timer
+    
+    let deadline ='2018-10-21';
+    function getTimeRemaining (endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor(t / (1000*60*60));
+        return {
+'total': t,
+'hours': hours,
+'minutes': minutes,
+'seconds': seconds
+        };
+
+    }
+function setClock(id, endtime){
+let timer = document.getElementById(id),
+hours = timer.querySelector('.hours'),
+minutes = timer.querySelector('.minutes'),
+seconds = timer.querySelector('.seconds'),
+timeInterval = setInterval(updateClock, 1000);
+function updateClock() {
+    let t = getTimeRemaining(endtime);
+    function addZero(num){
+        if(num <= 9) {
+            return '0' + num;
+        } else return num;
+    };
+    hours.textContent =addZero (t.hours);
+    minutes.textContent = addZero(t.minutes);
+    seconds.textContent = addZero(t.seconds);
+    if (t.total <= 0){
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+    }
+
+}
+
+
+}
+setClock('timer', deadline);
+// modal window
+let more = document.querySelector('.more'),
+overlay = document.querySelector('.overlay'),
+close = document.querySelector('.popup-close');
+more.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+});
+close.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+});
+// Form
+
+ let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach((value, key) => 
+        obj[key] = value);
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
+});
+// slider
+let slideIndex = 1,
+slides = document.querySelectorAll('.slider-item'),
+prev = document.querySelector('.prev'),
+next = document.querySelector('.next'),
+dotsWrap = document.querySelector('.slider-dots'),
+dots = document.querySelectorAll('.dot');
+showSlides(slideIndex);
+function showSlides (n) {     //n is slide's number
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    if(n < 1) {slideIndex = slides.length;}
+    slides.forEach((item) => item.style.display = 'none');
+    dots.forEach((item) => item.classList.remove('dot-active'));
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+}
+function plusSlides (n) {
+   showSlides(slideIndex +=n);
+}
+function currentSlide (n) {
+    showSlides(slideIndex = n);
+}
+prev.addEventListener('click', () => plusSlides(-1));
+next.addEventListener('click', () => plusSlides(1));
+dotsWrap.addEventListener('click', (event) =>{
+    for (let i =0; i < dots.length + 1; i++) {
+        if(event.target.classList.contains('dot')
+        && event.target == dots[i-1]) {
+            currentSlide(i);
+        }
+    }
+});
+// calc
+let persons = document.querySelectorAll('.counter-block-input')[0],
+restDays = document.querySelectorAll('.counter-block-input')[1],
+place = document.getElementById('select'),
+totalValue =document.getElementById('total'),
+personsSum =0,
+daysSum =0,
+total = 0;
+totalValue.innerHTML= 0;
+persons.addEventListener('change', function(){
+    personsSum = +this.value;
+    total = (daysSum + personsSum) *4000;
+    if(restDays.value == '') {
+        totalValue.innerHTML = 0;
+    }
+    else {
+        totalValue.innerHTML = total;  
+    }
+});
+restDays.addEventListener('change', function(){
+    daysSum = +this.value;
+    total = (daysSum + personsSum) *4000;
+    if(persons.value == '') {
+        totalValue.innerHTML = 0;
+    }
+    else {
+        totalValue.innerHTML = total;  
+    }
+});
+place.addEventListener('change', function(){
+    if(restDays.value == '' || persons.value == ''){
+        totalValue.innerHTML = 0;
+    }
+    else {
+        let a = total;
+        totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+});
+
+});
+
+
+
+
+
